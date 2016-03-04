a1a0a04ad0129cf8eb084f2c5f842ca26a1625bc
K Rain Leander <rainsdance@users.noreply.github.com>
2016-03-04 13:23:48 +0100

    Uploaded ps0.py assignment

---
+++ ps0.py
@@ -0,0 +1,12 @@
+# Problem Set 0
+# Name: K Rain Leander
+# Collaborators: n/a
+# Time: 0:15
+#
+print('Enter your last name:')
+last = raw_input('**')
+print('Enter your first name:')
+first = raw_input('**')
+print(first)
+print(last)
+
